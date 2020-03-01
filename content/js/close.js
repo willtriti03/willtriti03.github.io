2f00de029012cab1ab78729d4420916d57a79855
unknown <willtriti03@naver.com>
2020-03-01 10:48:27 +0900

    add close BTN

--- content/js/close.js
+++ content/js/close.js
@@ -1,7 +1,11 @@
-Array.from(document.getElementsByTagName('path')).map(path => {
-  console.log(path.getTotalLength());
-  const debugPath = path.cloneNode();
-  debugPath.classList.add('line--debug');
-  if (path.parentNode) path.parentNode.insertBefore(debugPath.cloneNode(), path);
-});
+var check = false;
 
+function btnClicked() {
+    if (!check) {
+        check = true;
+    } else {
+        check = false;
+        window.history.back();
+        
+    }
+}
