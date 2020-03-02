dd7ea23d3e6fe13458baea412767a39938f0d51b
unknown <willtriti03@naver.com>
2020-03-02 23:37:45 +0900

    add long press backer

---
+++ assets/js/close.js
@@ -0,0 +1,35 @@
+var timer;
+var time = 0;
+var isTrue = false;
+var bar = document.getElementById("closer");
+
+$(document).mousedown(function clicked() {
+    timer = setInterval(function ho(){holding();}, 1);
+    isTrue = true;
+    time=0;
+});
+$(document).mouseup(function released() {
+    isTrue = false;
+    clearInterval(timer);
+    bar.style.height = 0 + '%';
+});
+
+function holding() {
+    if (isTrue)
+        time += 1;
+    if(time >100){
+        bar.style.height = (time-100)/2 + '%';
+    }
+    
+    if (time > 300) {
+        time = 0;
+        clearInterval(timer);
+        longCl();
+    }
+    console.log(time);
+}
+
+function longCl() {
+    bar.style.height = 0 + '%';
+    history.back();
+}
