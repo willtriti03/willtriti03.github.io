509d61b4ca7a05f6c56dd9bb4938db0c2552aeda
willtriti <willtriti03@naver.com>
2021-04-24 18:13:31 +0900

    brackets down tmp save

--- content/assets/slide.js
+++ content/assets/slide.js
@@ -5,7 +5,7 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    
+   
     n = (slideIndex - 1)%slides.length;
     
     if(n<0){ 
@@ -26,6 +26,8 @@ function showSlides(n) {
     
     
     slides[n].style.display = "block";
+    slides[n].blur();
+
     //dots[n].className = dots[n].className + " activeDot";
        console.log("슬라이드 "+n);
 }
@@ -39,5 +41,12 @@ function plusSlides(n) {
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
+
+$(function() {
+var slides = document.getElementsByClassName("mySlides");
+  $(window).scroll(function() {
+    slides[slideIndex].blur();
+  });
+});
 
 
